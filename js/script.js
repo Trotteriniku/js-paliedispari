797898e5ed2c846b87d7efddd216282f25076a72
Trotteriniku <stefanozidda3@gmail.com>
2023-10-17 20:02:11 +0200

    'palindroma' exercise

--- js/script.js
+++ js/script.js
@@ -2,31 +2,39 @@
 const btn =document.querySelector('button');
 let palienNum;
 
-function palieCeck() {
-    
-} 
 
 btn.addEventListener('click',function() {
     palienNum =document.getElementById('text');
+    const alert =document.querySelector('.alert');
+    alert.classList.remove('d-none')
     let palieNumEl = palienNum.value
     let parola1 = ''
     let parola2 = ''
-    
+    let msg;
 
    for (let i = 0; i < palieNumEl.length; i++) {
-         parola1 += palieNumEl[i];
-         
+         parola1 += palieNumEl[i];   
     }
-    
    for (let j = palieNumEl.length -1; j >= 0; j--) {
         parola2 += palieNumEl[j];
    }
-   console.log(parola2);
 
-   parola1 === parola2 ? true: false;
-
+   if (parola1 === parola2) {
+        alert.classList.add('alert-success')
+        msg =`
+        Bravo ${parola1} è una parola palindroma!!
+        `
+        alert.innerHTML=msg
+   } else {
+        alert.classList.remove('alert-success')
+        alert.classList.add('alert-info')
+        msg =`
+        Mi spiace ma ${parola1} è una parola non  palindroma prova con parole come (osso,ingegni...etc)!!
+        `
+        alert.innerHTML=msg
+   }
    
-    
+        
    
 
 } 
